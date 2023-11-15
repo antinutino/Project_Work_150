78554eb28efa8d309c1726e2297825fafae9a5ee
antinutino <maharabopi73@gmail.com>
2023-11-15 13:51:52 +0600

    Drawing food

--- snake.cpp
+++ snake.cpp
@@ -8,6 +8,77 @@ int direction=1;
 int score=0;
 int rectenglex=100;
 int rectengley=100;
+void snakefood()
+{
+    cout<<"score:"<<score++<<endl;
+    foodX = rand() % (780 /10 ) * 10;
+    foodY = rand() % (580 / 10) * 10;
+    if(dir==1)
+    {
+         v.push_back( make_pair(recx+10,recy) );
+    }
+    else if(dir==2)
+    {
+         v.push_back( make_pair(recx-10,recy) );
+    }
+    else if(dir==3)
+    {
+         v.push_back( make_pair(recx,recy+10) );
+    }
+    else if(dir==4)
+    {
+         v.push_back( make_pair(recx,recy-10) );
+    }
+}
+void DrawRect(int x, int y) {
+    SDL_Rect rect = { x, y, 10, 10};
+    SDL_RenderFillRect(renderer, &rect);
+}
+void direction()
+{
+    if (event.type == SDL_KEYDOWN) 
+                {switch (event.key.keysym.sym) 
+                    {case SDLK_UP:
+                          { if(dir==3)
+                          continue;
+                          else
+                            {dir=4;
+                          break;}}
+                    case SDLK_DOWN:
+                        { if(dir==4)
+                        continue;
+                        else
+                        {dir=3;
+                      
+                        break;}
+                        }
+                    case SDLK_LEFT:
+                     { if(dir==1)
+                     continue;
+                     else
+                        {dir=2;
+                    
+                     break;}}
+                    case SDLK_RIGHT:
+                    { if(dir==2)
+                    continue;
+                    else
+                        {dir=1;
+                        break;}}
+
+    } }
+    }
+    void Drawfood()
+    {  
+        int rad=10;
+         while(rad--)
+    for(int angle=0;angle<360;angle++)
+    {
+        int x=foodX+rad*cos(angle*M_PI/180);
+         int y=foodY+rad*sin(angle*M_PI/180);
+         SDL_RenderDrawPoint(renderer,x,y);}
+    }
+
 SDL_Window* window=SDL_CreateWindow("Snake Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_SHOWN);
 SDL_Renderer* renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
 int main(int argc,char* argv[])
@@ -18,6 +89,7 @@ int main(int argc,char* argv[])
         return 1;
     }
 
+
 while(quit!=true)
 {
     SDL_Event event;
@@ -27,7 +99,11 @@ while(quit!=true)
 {
     quit=true;
 }
+     else 
+     direction();
     }
+     Drawfood();
+     
     SDL_SetRenderDrawColor(renderer,0,0,0,255);
      SDL_RenderClear(renderer);
      SDL_SetRenderDrawColor(renderer,10,100,200,255);
